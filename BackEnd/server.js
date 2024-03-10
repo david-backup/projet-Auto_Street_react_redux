06a4eb7dbcb77c54ffddd518ab5ab04ee366234b
David Changea <14ir155@gmail.com>
2024-03-11 00:06:50 +0100

    config et connection à la base de données auto-street sur mongoDB

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
 const port = 5000;
+
+// connection à la db
+connectDB();
 
 const app = express();
 
