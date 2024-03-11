46ba9c3a33b532e6bcf1cf25732494b213056b33
David Changea <14ir155@gmail.com>
2024-03-11 13:55:41 +0100

    reset backend

--- BackEnd/routes/delete.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.delete("/:id", (req, res) => {
-  res.json({ message: "post supprimé id " + req.params.id });
-});
-
-module.exports = router;
--- BackEnd/routes/post.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/", (req, res) => {
-  res.json({ message: req.body.message });
-});
-
-module.exports = router;
