984a2b0c6a4f6bbee3955bec944ca2f8018fcbf5
David Changea <14ir155@gmail.com>
2024-03-12 20:09:31 +0100

    variables de couleurs crées, début de navigation crée

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Navigation from "./components/Navigation";
 
 const App = () => {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
--- FrontEnd/src/components/Navigation.jsx
+++ FrontEnd/src/components/Navigation.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
-  return <header className="header"></header>;
+  return (
+    <header className="header">
+      <div className="header_title">
+        <h1>Garage Auto-Street</h1>
+        <Link
+          to="https://www.google.com/search?hl=fr-FR&amp;gl=fr&amp;q=AUTO-STREET,+5+Rue+de+Moraye,+26760+Beaumont-l%C3%A8s-Valence&amp;ludocid=17524552952016791104&amp;lsig=AB86z5UtpFnldOaCe7VJJ494I8jO#lrd=0x12b5530b638cdd8d:0xf333b80f907be640,1,,,,"
+          target="_blank"
+          className="button"
+        >
+          Voir les avis clients
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Navigation;
